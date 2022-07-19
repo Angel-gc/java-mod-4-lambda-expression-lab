5cfcb01db35d45726d956952f7f4e68a9ece4d0c
Angel Concepcion <angelconcepcion50@gmail.com>
2022-07-19 13:04:54 -0400

    solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,22 @@ import java.util.function.IntUnaryOperator;
 
 public class Main {
 	// write the nextOddNum here
-    
+    public static oddNumInterface nextOddNum = (n) -> {
+       if((n |1) > n){
+            return n+=1;
+       } else {
+          return n+=2;
+       }
+    };
     public static void main(String[] args) {
         int num = 5;
         int num2 = 10;
 
         System.out.println(nextOddNum.applyAsInt(num)); // 7
         System.out.println(nextOddNum.applyAsInt(num2)); // 11
+    }
+    @FunctionalInterface
+    interface oddNumInterface {
+        int applyAsInt(int num);
     }
 }
