fe078aee18db00163dd5687f5d933a8a9b93b174
Angel Concepcion <angelconcepcion50@gmail.com>
2022-07-19 13:22:04 -0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.util.function.IntUnaryOperator;
 
 public class Main {
 	// write the nextOddNum here
-    public static oddNumInterface nextOddNum = (n) -> {
+    public static IntUnaryOperator nextOddNum = (n) -> {
        if((n |1) > n){
             return n+=1;
        } else {
@@ -16,8 +16,8 @@ public class Main {
         System.out.println(nextOddNum.applyAsInt(num)); // 7
         System.out.println(nextOddNum.applyAsInt(num2)); // 11
     }
-    @FunctionalInterface
-    interface oddNumInterface {
-        int applyAsInt(int num);
-    }
+//    @FunctionalInterface
+//    interface oddNumInterface {
+//        int applyAsInt(int num);
+//    }
 }
